4244ff77dab2189bff753c213d0937f7cc68a0a2
kmerrell <kmerrell@gmail.com>
2014-09-26 16:51:13 -0500

    building out metricMakerSrvc

--- public/superViz/MetricMakerSrvc.js
+++ public/superViz/MetricMakerSrvc.js
@@ -5,27 +5,33 @@ appCreateReport.factory('MetricMakerSrvc',function(){
     "use strict";
     return{
         visualizationTypes:['linechart','verticalbarchart','horizontalbarchart','piechart','termcloud','map','text','image','donut'],
-        //todo: Legal visualizations are really more a function of the selected metric than the base datatype.
-        legalVisualizations:[
-            {ctdatatype:"address",legalvisualiations:["termcloud"]},
-            {ctdatatype:"alphanumber",legalvisualiations:["termcloud"]},
-            {ctdatatype:"barcode",legalvisualiations:["termcloud"]},
-            {ctdatatype:"boolean",legalvisualiations:["horizontalbarchart","verticalbarchart","piechart","donut"]},
-            {ctdatatype:"date",legalvisualiations:["linechart","piechart"]},
-            {ctdatatype:"geolocation",legalvisualiations:["linechart","piechart"]},
-            {ctdatatype:"group",legalvisualiations:["linechart","piechart"]},
-            {ctdatatype:"integer",legalvisualiations:["linechart","piechart"]},
-            {ctdatatype:"measurement1d",legalvisualiations:["linechart","piechart"]},
-            {ctdatatype:"measurement2d",legalvisualiations:["linechart","piechart"]},
-            {ctdatatype:"measurement3d",legalvisualiations:["linechart","piechart"]},
-            {ctdatatype:"multichoice",legalvisualiations:["linechart","piechart"]},
-            {ctdatatype:"multilinetext",legalvisualiations:["linechart","piechart"]},
-            {ctdatatype:"number",legalvisualiations:["linechart","piechart"]},
-            {ctdatatype:"percentage",legalvisualiations:["linechart","piechart"]},
-            {ctdatatype:"singlechoice",legalvisualiations:["linechart","piechart"]},
-            {ctdatatype:"singlelinetext",legalvisualiations:["linechart","piechart"]},
-            {ctdatatype:"title",legalvisualiations:["linechart","piechart"]}
+
+        preMadeMetrics:[
+            {name:'ListOfLocations',validVisualizations:["map","termcloud"],  description:'Simple list of the locations (address or geolocation) for all instances of a template. Format is {seriesname:"somename",["value"]}' },
+            {name:'ListOfResponses',validVisualizations:["termcloud"],  description:'Simple list of the values of a single question for all instances of a template. Format is {seriesname:"somename",["value"]}' },
+            {name:'CountOfValues',validVisualizations:["linechart","verticalbarchart","horizontalbarchart","piechart","termcloud","map","text","image","donut"],description:'The number of times each value occurs within all instances of a template (count aggragate). Format is {seriesname:"somename",[{value:"somevalue",count:"somecount"}]}'}
+        ],
+        
+        defaultVisualizations:[
+            {ctdatatype:"address", defaultmetric:"ListOfLocations", defaultvisualization:"map"},
+            {ctdatatype:"alphanumber", defaultmetric:"CountOfValues", defaultvisualization:"termcloud"},
+            {ctdatatype:"barcode", defaultmetric:"CountOfValues", defaultvisualization:"termcloud"},
+            {ctdatatype:"boolean", defaultmetric:"CountOfValues", defaultvisualization:"horizontalbarchart"},
+            {ctdatatype:"date", defaultmetric:"CountOfValues", defaultvisualization:"linechart"},
+            {ctdatatype:"geolocation", defaultmetric:"ListOfLocations", defaultvisualization:"map"},
+            {ctdatatype:"group", defaultmetric:"CountOfValues", defaultvisualization:""},
+            {ctdatatype:"integer", defaultmetric:"CountOfValues", defaultvisualization:"horizontalbarchart"},
+            {ctdatatype:"measurement1d", defaultmetric:"CountOfValues", defaultvisualization:""},
+            {ctdatatype:"measurement2d", defaultmetric:"CountOfValues", defaultvisualization:""},
+            {ctdatatype:"measurement3d", defaultmetric:"CountOfValues", defaultvisualization:""},
+            {ctdatatype:"multichoice", defaultmetric:"CountOfValues", defaultvisualization:"termcloud"},
+            {ctdatatype:"multilinetext", defaultmetric:"CountOfValues", defaultvisualization:"termcloud"},
+            {ctdatatype:"number", defaultmetric:"CountOfValues", defaultvisualization:"horizontalbarchart"},
+            {ctdatatype:"percentage", defaultmetric:"CountOfValues", defaultvisualization:"horizontalbarchart"},
+            {ctdatatype:"singlechoice", defaultmetric:"CountOfValues", defaultvisualization:"horizontalbarchart"},
+            {ctdatatype:"singlelinetext", defaultmetric:"CountOfValues", defaultvisualization:"termcloud"},
+            {ctdatatype:"title", defaultmetric:"CountOfValues", defaultvisualization:"termcloud"}
         ]
     }
-})
+});
 
