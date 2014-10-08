4355e2a9ec92ca60300e4f177eabd4fb63b07a47
kmerrell <kmerrell@gmail.com>
2014-10-07 19:20:12 -0500

    Major restyling

--- public/superViz/testChartContainer/testChartContainer.js
+++ public/superViz/testChartContainer/testChartContainer.js
@@ -9,10 +9,11 @@ appCreateReport.directive('testChartContainer', function () {
         scope: true,
         templateUrl: baseurl + '/visualizations/public/superViz/testChartContainer/test-chart-container.html',
         link: function (scope, el, attrs) {
-
+            scope.propertyBag = {};
             scope.mywidth = attrs.width;
             scope.myheight = attrs.height;
             scope.ctdatatype = attrs.ctdatatype;
+            scope.propertyBag.dimensionsSettings="height:"+scope.myheight+"px; width:"+scope.mywidth+"px;"
 
         }
     }
